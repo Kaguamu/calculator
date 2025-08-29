d31b4d235ea13bbddce7b7461b066689128e6357
Kaguamu <duncanmutiga7@gmail.com>
2025-08-29 14:43:28 +0300

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,10 @@
+//const no1=document.getElementById("no1").value;
+const results=document.getElementById("results");
+const input=document.getElementById("input");
+
+
+
+ no1.onclick=function(){
+    document.getElementById("no1").textContent=2;
+ }
+   
